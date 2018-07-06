a1fba13f4c802c11e88478884085d98cfd811332
zouhuan <939215506@qq.com>
2018-07-06 13:02:28 +0800

    第一次提交代码

---
+++ git_test44/src/cn/itcast/gitdemo/GitTest.java
@@ -0,0 +1,10 @@
+package cn.itcast.gitdemo;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("helloGit1");
+        System.out.println("helloGit2");
+        System.out.println("helloGit3");
+    }
+}
